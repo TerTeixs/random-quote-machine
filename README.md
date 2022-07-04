df376241835f862170510f67d623e88556b72893
peter@LAPTOP-Q27MJ883 <peter10228@msn.com>
2022-07-04 15:59:08 +0700

    completed frontend freecodecamp project

---
+++ README.md
@@ -0,0 +1,5 @@
+This is random quote machine for FreeCodeCamp.com's 
+front end library certfication project
+
+https://www.freecodecamp.org/learn/front-end-development-libraries/front-end-development-libraries-projects/build-a-random-quote-machine
+
---
+++ src/App.js
@@ -0,0 +1,58 @@
+import React ,{ useEffect, useState } from 'react';
+import './App.scss';
+
+let quoteDBUrl = "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json"
+
+function App() {
+  const [quotesArray , setQuotesArray] = useState({})
+
+  const fetchQuotes = async (url) => {
+    const response = await fetch(url)
+    const parsedJSON = await response.json()
+    setQuotesArray(parsedJSON.quotes)
+  }
+
+  useEffect(() => {
+    fetchQuotes(quoteDBUrl)
+  },[quoteDBUrl])
+
+  const [ randomNumber , setRandomNumber] = useState(0)
+  
+  const [ quote, setQuote ] = useState("")
+
+  const [ author, setAuthor ] = useState("")
+
+  const getRandomQuote = () => {
+    let RNG = Math.floor((quotesArray.length)*Math.random())
+    setRandomNumber(RNG)
+    setQuote(quotesArray[randomNumber].quote)
+    setAuthor(quotesArray[randomNumber].author)
+  }
+
+    return (
+    <div className="App">
+      <header className="App-header">
+        <section id="quote-box">
+          <div className="quote-author-box">
+          <p id="text">
+            <img id="quote-logo" alt="quote-left" src="quote-left.png"></img>
+            {quote}"
+          </p>
+          <p id="author">
+            - {author}
+          </p>
+          </div>
+          <div className='button-box'>
+            <button id="new-quote" onClick={()=> getRandomQuote()}>New Quote</button>
+            <a href={"https://twitter.com/intent/tweet?text=" + quote + " -" + author} target="_blank" id="tweet-quote">
+              <img id="logo-img"src="logo-twitter.ico" alt="twitter-logo"></img>
+            </a>
+          </div>
+
+        </section>
+      </header>
+    </div>
+  );
+}
+
+export default App;
